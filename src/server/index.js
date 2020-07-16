5245693a12f8614577b2304d932f348c347e9447
Edgar Melara <ed.melara.eg@gmail.com>
2020-07-16 12:43:31 -0600

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,18 @@
+/*
+* Dependencias
+*/
+
+var http = require('http')
+var express = require('express')
+var socketio = require('socket.io')
+
+var port = 8082
+var app = express()
+
+var server = http.createServer(app)
+
+app.use(express.static('public'))
+
+server.listen(port, function(){
+    console.log("Sever is running on port: "+port)
+})
